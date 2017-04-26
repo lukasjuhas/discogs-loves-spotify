91f51e4bb00e99682a10b131a60a97d4c9baaa5b
Lukas Juhas <hello@lukasjuhas.com>
2017-04-26 16:03:01 +0100

    discogs oauth in working condition

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -11,6 +11,50 @@ class SiteController extends Controller
 {
     public function index()
     {
+        $oauth_token = isset($_GET['oauth_token']) ? $_GET['oauth_token'] : false;
+        $oauth_verifier = isset($_GET['oauth_verifier']) ? $_GET['oauth_verifier'] : false;
+
+        if($oauth_token || $oauth_verifier) :
+            $stack = HandlerStack::create();
+
+            $middleware = new Oauth1([
+                'consumer_key'    => env('DISCOGS_KEY'),
+                'consumer_secret' => env('DISCOGS_SECRET'),
+                'token'           => $oauth_token,
+                'token_secret'    => session('discogs.oauth_token_secret'),
+                'verifier'        => $oauth_verifier,
+                // 'signature_method'=> 'PLAINTEXT'
+            ]);
+
+            $stack->push($middleware);
+
+            $config = [
+                'handler' => $stack,
+                'headers' => [
+                    'User-Agent' => 'discogslovesspotify/1.0.0 +http://discogslovesspotify.dev',
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+            ];
+
+            $client = new Client($config);
+            // dd($client);
+            $response = $client->request('POST', 'https://api.discogs.com/oauth/access_token', [
+                'auth' => 'oauth',
+            ]);
+
+            $params = [];
+            parse_str($response->getBody(), $params);
+
+            if($params) {
+                session([
+                    'discogs' => [
+                        'oauth_token' => $params['oauth_token'],
+                        'oauth_token_secret' => $params['oauth_token_secret'],
+                    ]
+                ]);
+            }
+        endif;
+
         return view('home');
     }
 
@@ -22,7 +66,8 @@ class SiteController extends Controller
             'consumer_key'    => env('DISCOGS_KEY'),
             'consumer_secret' => env('DISCOGS_SECRET'),
             'token'           => '',
-            'token_secret'    => ''
+            'token_secret'    => '',
+            'callback'        => 'http://discogslovesspotify.dev',
         ]);
 
         $stack->push($middleware);
@@ -37,12 +82,18 @@ class SiteController extends Controller
 
         $client = new Client($config);
         $response = $client->request('GET', 'https://api.discogs.com/oauth/request_token', [
-            'auth' => 'oauth'
+            'auth' => 'oauth',
         ]);
 
         $params = [];
         parse_str($response->getBody(), $params);
 
-        print_r($params);
+        session(['discogs' => [
+            'oauth_token' => $params['oauth_token'],
+            'oauth_token_secret' => $params['oauth_token_secret'],
+        ]]);
+
+        return redirect('https://discogs.com/oauth/authorize?oauth_token=' . $params['oauth_token']);
+
     }
 }
